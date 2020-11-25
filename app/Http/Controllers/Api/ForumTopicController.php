887a1e778cc292f3822938b1eb4cc1d8d9472a6d
Adesanoye Samson <adesanoye.samson@yahoo.com>
2020-11-25 17:45:11 +0100

    initial controller for ForumCategory ForumComment ForumTopic

---
+++ app/Http/Controllers/Api/ForumTopicController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\ForumTopic;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ForumTopicController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ForumTopic  $forumTopic
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ForumTopic $forumTopic)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ForumTopic  $forumTopic
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ForumTopic $forumTopic)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ForumTopic  $forumTopic
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ForumTopic $forumTopic)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ForumTopic  $forumTopic
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ForumTopic $forumTopic)
+    {
+        //
+    }
+}
